fb888b12a956c9abc47dd010c187b33e3e38d743
Nathan Kot <me@nathankot.com>
2015-09-23 03:36:56 +0900

    Apply carthage support

---
+++ SpecificationPattern/SpecificationPattern.h
@@ -0,0 +1,19 @@
+//
+//  SpecificationPattern.h
+//  SpecificationPattern
+//
+//  Created by Nathan Kot on 23/09/15.
+//  Copyright © 2015 Nathan Kot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SpecificationPattern.
+FOUNDATION_EXPORT double SpecificationPatternVersionNumber;
+
+//! Project version string for SpecificationPattern.
+FOUNDATION_EXPORT const unsigned char SpecificationPatternVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpecificationPattern/PublicHeader.h>
+
+
